f1524067c16f52e86cb7c4ec39b8c67bc3363804
Andrei Chepelev <kirkkonen@gmail.com>
2020-12-30 20:34:47 +0200

    selected items are deleted from the list on click, button appears only if smth is selected

--- src/app/subscription-list/subscription-list.component.ts
+++ src/app/subscription-list/subscription-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSelectionList } from '@angular/material/list';
+
 
 @Component({
   selector: 'app-subscription-list',
@@ -7,16 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscriptionListComponent implements OnInit {
 
-  appList: string[] = ['Instagram', 'Facebook', 'Telegram', 'WhatsApp', 'GitHub'];
 
-  // deleteSelected() {
-  //   console.log(selectionList.selectedOptions.selected.length)
-  // }
-  
+  @ViewChild('apps') selectionList: MatSelectionList
 
-  // deleteSelected() {
-  //   console.log(this.appList.selectedOptions.selected.map(s => s.value))
-  // }
+  areSelected = false
+
+  onSelectionChange() {
+    if (this.selectionList.selectedOptions.selected.length > 0) {
+      this.areSelected = true
+    }
+    else {
+      this.areSelected = false
+    }
+  }
+
+  appList: string[] = ['Instagram', 'Facebook', 'Telegram', 'WhatsApp', 'GitHub']
+
+  deleteSelected() {
+    var selectedList: string[] = this.selectionList.selectedOptions.selected.map(s => s.value)
+    var diff = this.appList.filter(el => !selectedList.includes(el))
+    this.appList = diff
+  }
 
   constructor() { }
 
