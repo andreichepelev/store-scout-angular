d7029afe3bc0d9433f82c8ca9158cc3c9aead24a
Andrei Chepelev <kirkkonen@gmail.com>
2021-10-01 08:07:05 +0300

    add updated date

--- src/app/jobs-admin-panel/jobs-admin-panel.component.ts
+++ src/app/jobs-admin-panel/jobs-admin-panel.component.ts
@@ -72,69 +72,18 @@ export class JobsAdminPanelComponent implements OnInit, OnDestroy {
       'zeroSubscribers', 
       'backendResult', 
       'puppeteerChromeResult', 
-      'puppeteerFirefoxResult', 
       'newNotes', 
       'reportSent', 
       'dbUpdated', 
       'invalid', 
       'scheduled', 
-      'sentToWS'
+      'sentToWS',
+      'updatedAt'
     ];
   //table data exported:
   table_data = this.jobData;
 
 
-//   getData(): Observable<ServerResponse> {
-//     const allOperations = forkJoin(
-//         this.getClientData(),
-//         this.getOtherData()
-//     );
-
-//     const observable = Observable.create(function subscribe(observer) {
-//         // Wait until all operations have completed
-//         allOperations.subscribe(([clientData, otherData]) => {
-//             const data = new ServerResponse;
-//             // Update your ServerReponse with client and other data
-//             data.otherdata = otherData.other;
-//             data.client = clientData.client;
-//             // Now that data is 100% populated, emit to anything subscribed to getData().
-//             observer.next(data);
-//             observer.complete();
-//         });
-
-//     });
-//     // We return the observable, with the code above to be executed only once it is subscribed to
-//     return observable;
-// }
-
-//   getJobs(): Observable<ServerResponse> {
-//     console.log('Trying to get jobs')
-//     const observable = new Observable(
-//       function subscribe(observer) {
-        
-//       }
-//     )
-    
-
-//         return this.http.get<JobResult>(
-//           this.jobServerUrl, 
-//           {
-//             withCredentials: true,
-//             observe: 'body',
-//             responseType: 'json'
-//           }
-//         )    
-//       }),
-//       catchError(error => {
-//         console.log('Getting app data failed')
-//         return throwError(error)
-//       })
-//     ).subscribe((data) => {
-//       console.log('got list: ', data)
-//       data.forEach(element => this.table_data.push(element));
-//       console.log('table data: ', this.table_data)
-//     })
-//   }
 
   getJobs() {
     console.log('Trying to get jobs')
@@ -150,34 +99,6 @@ export class JobsAdminPanelComponent implements OnInit, OnDestroy {
         console.log('job result: ', jobResult)
         return jobResult
   }
-
-  // getJobs() {
-  //   console.log('Trying to get jobs')
-  //   this.authService.authState.pipe(
-  //     tap((state) => console.log('[Jobs list] authState', state)),
-  //     filter((state) => state),
-  //     switchMap(() => {
-  //       console.log('[Jobs list] Getting jobs')
-  //       const jobResult = this.http.get<JobResult>(
-  //         this.jobServerUrl, 
-  //         {
-  //           withCredentials: true,
-  //           observe: 'body',
-  //           responseType: 'json'
-  //         }
-  //       )
-  //       return jobResult
-  //     }),
-  //     catchError(error => {
-  //       console.log('Getting app data failed')
-  //       return throwError(error)
-  //     })
-  //   ).subscribe((data) => {
-  //     console.log('got list: ', data)
-  //     this.table_data = data
-  //     console.log('table data: ', this.table_data)
-  //   })
-  // }
 
 
   //for responsiveness
